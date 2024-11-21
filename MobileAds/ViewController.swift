c0d18f0f886c096a43fae67bd1464b32e4406b37
Nariman05Manas <89285569904@mail.ru>
2024-11-21 17:03:49 +0300

    new commit

--- MobileAds/ViewController.swift
+++ MobileAds/ViewController.swift
@@ -6,12 +6,42 @@
 //
 
 import UIKit
+import YandexMobileAds
 
 class ViewController: UIViewController {
-
+    
+    private lazy var adView: AdView = {
+        let width = view.safeAreaLayoutGuide.layoutFrame.width
+        let adSize = BannerAdSize.stickySize(withContainerWidth: width)
+        
+        let adView = AdView(adUnitID: "demo-appopenad-yandex", adSize: adSize)
+        adView.delegate = self
+        adView.translatesAutoresizingMaskIntoConstraints = false
+        return adView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        adView.loadAd()
+    }
+    
+    func showAd() {
+          view.addSubview(adView)
+          NSLayoutConstraint.activate([
+            adView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+              adView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+          ])
+      }
+}
+
+        extension ViewController: AdViewDelegate {
+            func adViewDidLoad(_ adView: AdView) {
+                showAd()
+            }
+
+            func adViewDidFailLoading(_ adView: AdView, error: Error) {
+                // This method will call after getting any error while loading the ad
+            
     }
 
 
